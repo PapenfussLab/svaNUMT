72f87911fce5c5355d1a5c41fc0f9a0e7c5bcac1
jackieduckie <lnyidrn@gmail.com>
2019-04-05 12:47:44 +1100

    update breakpointgr2bedpe

--- R/plotting.R
+++ R/plotting.R
@@ -1,7 +1,17 @@
-#' \code{breakpointgr2bedpe} Converts a breakpoint GRanges to a BEDPE-formatted data frame
-#' @param gr GRanges object
-#'
-#' @return BEDPE-formatted data frame
+#' Converting breakpoint GRanges to BEDPE-like dataframe
+#' @details
+#' \code{breakpointgr2bedpe} converts a breakpoint GRanges to a BEDPE-formatted
+#' dataframe. The BEDPE format consists of two sets of genomic loci, optinal
+#' columns of name, score, strand1, strand2 and any user-defined fields.
+#' See \url{https://bedtools.readthedocs.io/en/latest/content/general-usage.html}
+#' for more details of BEDPE format.
+#' @param gr A GRanges object.
+#' @return A BEDPE-formatted data frame.
+#' @examples
+#' vcf.file <- system.file("extdata", "gridss.vcf", package = "StructuralVariantAnnotation")
+#' vcf <- VariantAnnotation::readVcf(vcf.file, "hg19")
+#' gr <- breakpointRanges(vcf)
+#' breakpointgr2bedpe(gr)
 #' @export
 breakpointgr2bedpe <- function(gr){
 	bedpe <- data.frame(
@@ -17,46 +27,7 @@ breakpointgr2bedpe <- function(gr){
 		strand1=strand(gr),
 		strand2=strand(partner(gr))
 	)
-	l <- mapply(c, bedpe$name, bedpe$partner.name, SIMPLIFY = FALSE)
-	ll <- mapply(sort, l, SIMPLIFY = FALSE)
-	bedpe <- bedpe %>% dplyr::mutate(pair = as.character(ll)) %>%
-		dplyr::distinct(pair,.keep_all = TRUE) %>% dplyr::select(-c('partner.name','pair'))
-	#bedpe <- bedpe[stringr::str_detect(bedpe$name, "gridss.+o"),]
+	bedpe[(as.character(bedpe$chrom1)<as.character(bedpe$chrom2)) |
+		  	(bedpe$chrom1==bedpe$chrom2 & bedpe$start1<bedpe$start2),-c(8)]
 	return(bedpe)
 }
-
-#' @param bedpe BEDPE-formatted dataframe
-#' @param ... plotting parameters for circlize::circos.initializeWithIdeogram() and circlize::circos.genomicLink()
-#' @return circos plot with connected breakpoints
-bedpe2circos <- function(bedpe,
-						 cytoband = system.file(
-						 	package = "circlize","extdata", "cytoBand.txt"),
-						 species = NULL, sort.chr = TRUE,
-						 chromosome.index = NULL, major.by = NULL,
-						 plotType = c("ideogram", "axis", "labels"),
-						 track.height = NULL,
-						 ideogram.height = circlize::convert_height(2, "mm"),
-						 rou = circlize:::get_most_inside_radius(),
-						 rou1 = rou, rou2 = rou,
-						 col = "black", lwd = par("lwd"),
-						 lty = par("lty"), border = col, ...){
-	`%<a-%` <- pryr::`%<a-%`
-	bed1<-bedpe[,1:3]
-	bed2<-bedpe[,4:6]
-	circos.pryr %<a-% {
-		circlize::circos.initializeWithIdeogram(
-			cytoband = cytoband,
-				species = species, sort.chr = sort.chr,
-				chromosome.index = chromosome.index, major.by = major.by,
-				plotType = plotType,
-				track.height = track.height, ideogram.height = ideogram.height, ...)
-		circlize::circos.genomicLink(bed1, bed2, rou = rou,
-									 rou1 = rou1, rou2 = rou2,
-									 col = col, lwd = lwd,
-									 lty = lty, border = border, ...)
-
-	}
-	circlize::circos.clear()
-	return(circos.pryr)
-}
-
