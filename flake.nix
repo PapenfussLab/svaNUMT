5715a2fc3cc1c30b82392907d3b057e0b5e52132
Justin Bedo <cu@cua0.org>
2021-05-04 09:00:05 +1000

    nix: add BiocCheck phase

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     flake-utils.lib.eachDefaultSystem (system:
       with import nixpkgs { inherit system; };
       with rPackages; {
-        defaultPackage = buildRPackage {
+        defaultPackage = (buildRPackage {
           name = "NUMTDetect";
           src = ./.;
           propagatedBuildInputs = [
@@ -23,7 +23,21 @@
             S4Vectors
             GenomicFeatures
           ];
-        };
-
+        }).overrideAttrs (_: {
+          doCheck = true;
+          checkInputs = [ BiocCheck ];
+          script = writeText "bioc-check.r" ''
+            library(BiocCheck)
+            BiocCheck(paste0(Sys.getenv("TMPDIR"), "/NUMTDetect")
+              , `no-check-deprecated`=T  # Requires net
+              , `no-check-CRAN`=T        # Requires net
+              , `no-check-version-num`=T # No idea why this fails
+              , `quit-with-status`=T)
+          '';
+          checkPhase = ''
+            cp -r $PWD $TMPDIR/NUMTDetect
+            Rscript $script
+          '';
+        });
       });
 }
